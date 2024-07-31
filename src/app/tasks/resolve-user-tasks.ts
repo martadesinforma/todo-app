01e69f3d22f6534f9b4ba170c8e26de79a2ae22b
Marta Ocana <martacyta2021@gmail.com>
2024-07-31 19:34:46 +0200

    create resolve user tasks component

---
+++ src/app/tasks/resolve-user-tasks.ts
@@ -0,0 +1,22 @@
+
+import {  inject } from '@angular/core';
+import { Task } from './task/task.model';
+import { TasksService } from './tasks.service';
+import { ActivatedRouteSnapshot, ResolveFn, RouterLink, RouterState } from '@angular/router';
+
+export const resolveUserTasks: ResolveFn<Task[]> = (ActivatedRouteSnapshot, RouterState) => {
+
+  const order = ActivatedRouteSnapshot.queryParams['order']
+
+  const tasksService = inject(TasksService);
+
+  const tasks = tasksService
+    .allTasks()
+    .filter((task) => task.userId === ActivatedRouteSnapshot.paramMap.get('userId')) //userTasks contiene una lista de tareas filtradas. Solo se incluyen las tareas cuyo userId coincide con el userId del usuario actual.
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1)) //Si el orden no es descendente (por lo tanto, es ascendente), y la id de la tarea a es mayor que la id de la tarea b, coloca b antes que a devolviendo 1. De lo contrario, coloca a antes que b devolviendo -1.
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1)) //Si el orden es descendente y la id de la tarea a es mayor que la id de la tarea b, coloca a antes que b devolviendo -1. De lo contrario, coloca b antes que a devolviendo 1. El id de cada tarea es único basado en el timestamp actual
+  }
+  return tasks.length ? tasks : [];
+}

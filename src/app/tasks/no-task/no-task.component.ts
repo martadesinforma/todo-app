8fec1145eb1a12be76eb096dd35246234ca95b83
Marta Ocana <martacyta2021@gmail.com>
2024-07-31 11:05:57 +0200

    update no task component

--- src/app/tasks/no-task/no-task.component.ts
+++ src/app/tasks/no-task/no-task.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-no-task',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './no-task.component.css'
 })
 export class NoTaskComponent {
+  message = input.required<string>(); //esta propiedad tiene el nombre de message pq la clave de la propiedad data  que hemos definido en el app.routes.ts para la ruta vacia '',  se llama message: data: { message: 'Select a user to see their tasks!'}. Estos dos nombres tienen que coincidir.  Para conseguir esto, en el app.config.ts hemos importado withComponentInputBinding(). Esta función habilita la vinculación automática de los parámetros  de la ruta a las propiedades del componente usando decoradores de entrada (input binding).
 
 }
+

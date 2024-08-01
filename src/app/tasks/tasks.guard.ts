247202d11734abe8c54c9f3b64192ea263e43a7c
Marta Ocana <martacyta2021@gmail.com>
2024-08-01 19:01:52 +0200

    create tasks guard

---
+++ src/app/tasks/tasks.guard.ts
@@ -0,0 +1,11 @@
+import { CanDeactivateFn } from "@angular/router";
+import { NewTaskComponent } from "./new-task/new-task.component";
+
+
+//La función canLeaveEditPage está definida para ser usada como un guardia CanDeactivate y aplica específicamente al componente NewTaskComponent.
+export const canLeaveEditPage: CanDeactivateFn<NewTaskComponent> = (component) => { //component representa la instancia del componente que se está tratando de desactivar. En este caso, es una instancia de NewTaskComponent.
+  if (component.enteredTitle() || component.enteredDate() || component.enteredSummary()) { //si alguna de estas propiedades es vedadera, significa que contiene datos que han sido insertados por el usuario mediante el formulario, y entonces cuando pulsamos en el botón de 'cancel', preguntaremos mediante un  mensaje si queremos abandonar la página,  que nos devolvera true si el usuario pulsa 'aceptar'  o false si el usuario pulsa 'cancelar'. Si devolvemos true el usuario puede salir de la pagina pero si devolvemos false no podrá salir de la página
+    return window.confirm('Do you really want to leave? You will lose the entered data') //confirm() devolverá un boolean dependiendo de si el usuario ha pulsado 'aceptar' o 'cancelar' en la ventana que se abre con el mensaje
+  }
+  return true //si no hemos introducido datos y las propiedades enteredTitle(), enteredDate() y enteredSummary() del formulario del componente NewTaskComponent están sin modificar, cuando pulsemos en botón 'cancel' devolvemos true. Si devolvemos true el usuario puede salir de la pagina.
+}

b69f5891f6b9b150627bd5cb59b34db6cabb2eb7
Marta Ocana <martacyta2021@gmail.com>
2024-08-05 10:44:19 +0200

    update app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import {  UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import {  TasksComponent } from './tasks/tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { resolveUserName } from './users/resolve-user-name';
-import { resolveUserTasks } from './tasks/resolve-user-tasks';
 import { resolveTitle } from './users/resolve-title';
-import { canLeaveEditPage } from './tasks/tasks.guard';
+
 
 export const routes: Routes = [
   {
@@ -26,26 +23,7 @@ export const routes: Routes = [
       userName: resolveUserName //el valor debe apuntar a la funcion de resolución pero sin ser llamada
     },
     title: resolveTitle, //ESte título se establecerá dinamicamente.
-    children: [
-      {
-        path: '', //path: '' significa cuando no se especifica ninguna subruta, es decir,  cuando se accede a la ruta users/:userId sin ninguna ruta hija adicional
-        redirectTo:'tasks',
-        pathMatch: 'full' //asegura que la redirección a tasks solo ocurra si no hay ningún otro segmento en la URL después de users/:userId.
-      },
-      {
-        path: 'tasks', //esto significa que cuando la URL coincide con http://localhost:52951/users/u1/tasks, http://localhost:52951/users/u2/tasks... Angular debe mostrar el TasksComponent
-        component: TasksComponent, //Sin embargo, Angular necesita saber dónde en el DOM debe insertar este componente cuando la url sea  http://localhost:52951/users/u1/tasks, http://localhost:52951/users/u2/tasks...  Aquí es donde entra en juego router-outlet. En este caso lo vamos a insertar en el user-tasks.component.html para que se muestre en la pagina padre
-        resolve: { //la propiedad resolve se utiliza para pre-cargar datos necesarios antes de que se active una ruta y se muestre el componente correspondiente. Esta propiedad se configura con resolvers, que son servicios o funciones que obtienen estos datos.
-          userTasks :  resolveUserTasks, //el valor debe apuntar a la funcion de resolución pero sin ser llamada. Esta función de resolucion necesita parámetros de consulta (queryParams) para obtener las tareas del usuario seleccionado. El comportamiento por defecto de Angular es que las funciones de resolución solo se vuelven a ejecutar si cambia un parámetro de la ruta, pero no si cambia un queryParam (parámetro de consulta). Sin embargo, este comportamiento se puede cambiar añadiendo otra propiedad a la configuración de la ruta llamada runGuardsAndResolvers y asignándole como valor 'paramsOrQueryParamsChange' o 'always
-        },
-        runGuardsAndResolvers: 'always',
-      },
-      {
-        path: 'tasks/new', //esto significa que cuando la URL coincide con http://localhost:52951/users/u1/tasks/new, http://localhost:52951/users/u2/tasks/new... Angular debe mostrar el NewTaskComponent
-        component: NewTaskComponent, //Sin embargo, Angular necesita saber dónde en el DOM debe insertar este componente cuando la url sea  http://localhost:52951/users/u1/tasks/new, http://localhost:52951/users/u2/tasks/new...  Aquí es donde entra en juego router-outlet. En este caso lo vamos a insertar en el user-tasks.component.html para que se muestre en la pagina padre
-        canDeactivate: [canLeaveEditPage]//Es un guard. La idea detrás de poder desactivar una ruta es que podemos controlar si a un usuario se le permite salir de una página o no
-      }
-    ],
+    loadChildren: ()=> import('./users/users.routes').then(module=> module.routes)
   },
   // si cuando ingresas una URL que no existe, tu aplicación redirige a la url base http://localhost:4200, es porque no tienes una ruta que maneje URLs no definidas. Debes agregar una ruta comodín (path: '**') al final de tu configuración de rutas para manejar estas URLs no definidas:
   {
